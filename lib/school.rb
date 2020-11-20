26aad2eff98400974b71b0ac8a774c6f1fbe2f8c
Vincent Ivos <vincentivos1@gmail.com>
2020-11-19 20:02:52 -0500

    commit tree 14ab11e9c600227ccc30c8be23424cffe075af98

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+
+class School
+
+
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end 
+
+    def roster
+        @roster
+    end 
+
+    def grade(score)
+        @roster[score]
+    end
+
+    def add_student(student, score)
+        if @roster[score] != nil
+          @roster[score] << student
+        else
+          @roster[score] = [student]
+        end
+      end
+
+    def sort
+        @roster.each do |x,y|
+            y.sort!
+        end
+    end
+end
+
